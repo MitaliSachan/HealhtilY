d03a70869da41963e2fa54bde56601432b49350b
MitaliSachan <95533074+MitaliSachan@users.noreply.github.com>
2024-10-07 21:08:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyAPj1EbvWiGR6E1JCwNfW7qrhM7Ih3OEH8");
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const app = express();
